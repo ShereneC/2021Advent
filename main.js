565644036ba7fe21316997b4ca774f679874c98d
ShereneC <spyhopper@cableone.net>
2021-12-14 12:33:47 -0700

    stuck on reading the input have to install requir?

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
-var text = fs.readFileSync("./input1.txt", 'utf-8');
+var text = readFileSync("input1.txt", 'utf-8');
 var textByLine = text.split('\n')
 console.log('The Length of the array is,' + textByLine.length)
