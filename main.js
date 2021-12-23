0d4c34eb402df5a7c79d4170c38f6033682cd0f6
ShereneC <spyhopper@cableone.net>
2021-12-23 11:01:25 -0700

    grrrrr

--- main.js
+++ main.js
@@ -3116,8 +3116,8 @@ function day3Binary2() {
       if (lines.length > 1) {
         if (lines[k][m] != mostFrequent) {
           lines.splice(k, 1)
-          k--
           m++
+          k--
         }
       }
     }
